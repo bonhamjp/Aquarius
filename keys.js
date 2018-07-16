84fecf8e711e1277d2913e9ab819a54c4e0ae104
Kyle Wollman <wollmank@oregonstate.edu>
2018-07-16 21:28:03 +0000

    added ip to keys

--- keys.js
+++ keys.js
@@ -1,11 +1,10 @@
-//TODO Complete clientID and clientSecret with proper data
 module.exports = {
     google: {
-        clientID: "",
-        clientSecret: ""
+        clientID: "163229689204-jllnkm3ig1hhj9jvsq9j69engir5bvbu.apps.googleusercontent.com",
+        clientSecret: "gB3ZpwkeM3PNqiwP5kjVAlE3"
     },
     aquarius: {
-        domain: "",
-        port: ""
+        domain: "35.197.110.108",
+        port: "3000"
     }
 };
