1551626c249c37f61364d8ea240e83eb60c26db0
wollmank <wollmank@oregonstate.edu>
2018-07-16 17:39:30 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 module.exports = {
     google: {
-        clientID: "163229689204-jllnkm3ig1hhj9jvsq9j69engir5bvbu.apps.googleusercontent.com",
-        clientSecret: "gB3ZpwkeM3PNqiwP5kjVAlE3"
+        clientID: "",
+        clientSecret: ""
     },
     aquarius: {
-        domain: "35.197.110.108",
-        port: "3000"
+        domain: "",
+        port: ""
     }
 };
