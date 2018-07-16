d0c3a23df69de3fcf8046b043e57da32fcaeaf50
Ben <bencarlson54@gmail.com>
2018-07-16 23:08:41 +0000

    updated keys.js

--- keys.js
+++ keys.js
@@ -1,11 +1,11 @@
 //TODO Complete clientID and clientSecret with proper data
 module.exports = {
     google: {
-        clientID: "80409453417-kvp4mii4i02jtnlhekgsh97kmeq9137f.apps.googleusercontent.com",
-        clientSecret: "IKDvtRnWXmiAyknMW5A_JjSr"
+        clientID: "",
+        clientSecret: ""
     },
     aquarius: {
-        domain: "aquarius.farted.net",
-        port: "3000"
+        domain: "",
+        port: ""
     }
 };
