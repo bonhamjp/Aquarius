0848af39b9186d24fb962156d7455097f9cff7a9
bonhamjp <bonhamjp@gmail.com>
2018-07-19 22:37:36 -0700

    Fixe irregular spacing

--- dir-tree.js
+++ dir-tree.js
@@ -17,13 +17,13 @@ module.exports = {
 
     if (stats.isDirectory()) {
       info.folder = true;
-  		info.lazy = true; // FancyTree param to only expand node when clicked
+      info.lazy = true; // FancyTree param to only expand node when clicked
       info.children = fs.readdirSync(fileName).map(function(child) {
         return self.buildTreeData(fileName + "/" + child, key + 1);
       });
 
-  		// sort children putting directories on top
-  		info.children.sort(function(a, b) {
+      // sort children putting directories on top
+      info.children.sort(function(a, b) {
         if (a.folder === true && b.folder === false) return -1;
         if (b.folder === true && a.folder === false) return 1;
 
